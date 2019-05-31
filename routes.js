81c2952d72251c4bc40645245026e238dc6f1784
sieem <siem.linx@gmail.com>
2019-05-31 14:32:58 +0200

    forgot jwt in controller

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 
 const planningController = require('./controllers/planningController')
 const authController = require('./controllers/authController')
