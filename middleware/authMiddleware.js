2c8a49bf25b975a0f60afaaf5c551b487d453448
sieem <siem.linx@gmail.com>
2019-06-09 15:12:26 +0200

    rewriting the system to use tokens instead of cookies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,18 +4,18 @@ const User = require('../models/user')
 const secretKey = 'secretKey'
 
 exports.verifyToken = (req, res, next) => {
-    // if (!req.cookies.token) {
-    //     return res.status(401).send('Unauthorized request')
-    // }
-    // let token = req.cookies.token
-    // if (token === 'null') {
-    //     return res.status(401).send('Unauthorized request')
-    // }
-    // let payload = jwt.verify(token, secretKey)
-    // if (!payload) {
-    //     return res.status(401).send('Unauthorized request')
-    // }
-    // req.userId = payload.subject
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        return res.status(401).send('Unauthorized request')
+    }
+    let payload = jwt.verify(token, secretKey)
+    if (!payload) {
+        return res.status(401).send('Unauthorized request')
+    }
+    req.userId = payload.subject
     next()
 }
 
