1a89adaa910ac58b7cbf347c1e00c69387a6751d
Siem <siem@adsomenoise.com>
2019-07-10 22:51:54 +0200

    fix return value

--- controllers/companiesController.js
+++ controllers/companiesController.js
@@ -10,7 +10,7 @@ exports.saveCompany = (req, res) => {
     let company = new Company(req.body)
     Company.findByIdAndUpdate(company._id, company, { upsert: true }, function (err, savedCompany) {
         if (err) console.log(err)
-        else res.status(200).json(savedCompany)
+        else res.status(200).json(company)
     })
 }
 
