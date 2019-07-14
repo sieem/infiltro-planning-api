b03e678b513ca40bc3ed929984a170463cf296d1
Siem Lasseel <siem.linx@gmail.com>
2019-07-14 23:50:36 +0200

    editUser route + securing all admin routs

--- routes.js
+++ routes.js
@@ -10,19 +10,20 @@ const companiesController = require('./controllers/companiesController')
 
 const authMiddleware = require('./middleware/authMiddleware')
 
-router.post('/add-user', authMiddleware.verifyToken, authMiddleware.getUserDetails, upload.none(), authController.addUser)
+router.post('/add-user', upload.none(), authMiddleware.verifyToken, authMiddleware.getUserDetails, authController.addUser)
 router.post('/register', upload.none(), authController.registerUser)
 router.post('/login', upload.none(), authController.loginUser)
-router.get('/get-users', authController.getUsers)
+router.get('/get-users', authMiddleware.verifyToken, authMiddleware.getUserDetails, authController.getUsers)
 router.get('/get-user/:userId', authController.getUser)
+router.post('/update-user', upload.none(), authMiddleware.verifyToken, authMiddleware.getUserDetails, authController.editUser)
 
 router.get('/get-projects', authMiddleware.verifyToken, authMiddleware.getUserDetails, projectController.getProjects)
 router.get('/get-project/:projectId', authMiddleware.verifyToken, authMiddleware.getUserDetails, projectController.getProject)
-router.post('/save-project', authMiddleware.verifyToken, authMiddleware.getUserDetails, upload.none(), projectController.saveProject)
+router.post('/save-project', upload.none(), authMiddleware.verifyToken, authMiddleware.getUserDetails, projectController.saveProject)
 router.delete('/remove-project/:projectId', authMiddleware.verifyToken, authMiddleware.getUserDetails, projectController.removeProject)
 
-router.get('/get-companies', companiesController.getCompanies)
-router.post('/save-company', upload.none(), companiesController.saveCompany)
+router.get('/get-companies', authMiddleware.verifyToken, authMiddleware.getUserDetails, companiesController.getCompanies)
+router.post('/save-company', upload.none(), authMiddleware.verifyToken, authMiddleware.getUserDetails, companiesController.saveCompany)
 router.delete('/remove-company/:companyId', authMiddleware.verifyToken, authMiddleware.getUserDetails, companiesController.removeCompany)
 
 module.exports = router;
