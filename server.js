a2701943ae328e87cb361a882007b395424fa234
Siem <siem@adsomenoise.com>
2019-07-14 16:59:45 +0200

    adding https server with new certificates

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
+const http = require('http')
+const https = require('https')
 const fs = require('fs')
 const cookieParser = require('cookie-parser')
 const routes = require('./routes')
@@ -46,6 +48,36 @@ app.use(function (req, res, next) {
 
 app.use(express.static(staticRoot, { dotfiles: 'allow' } ));
 
-app.listen(port, () => {
-    console.log(`Server running on localhost:${port}`)
-})
+//starting up the server
+if (process.env.NODE_ENV == "production") {
+    // Certificate
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/planning.infiltro.be/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/planning.infiltro.be/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/planning.infiltro.be/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(process.env.PORT || 3000, err => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`App listening on port ${httpsServer.address().port} SSL`);
+    });
+}
+else if (process.env.NODE_ENV == "development") {
+    const httpServer = http.createServer(app);
+    httpServer.listen(process.env.PORT || 3000, err => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`App listening on port ${httpServer.address().port} Non-SSL`);
+    });
+}
+else {
+    throw new Error("No NODE_ENV defined, perhaps run npm start of npm run dev?");
+}
