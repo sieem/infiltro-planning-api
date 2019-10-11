9b7120d09b0f4ecc3d9ec2878c5c8288c593df0b
sieem <siem.linx@gmail.com>
2019-10-11 20:30:54 +0200

    removal of unneeded code

--- services/calendarService.js
+++ services/calendarService.js
@@ -1,12 +1,5 @@
 const fs = require('fs-extra');
-const readline = require('readline');
 const { google } = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
 const CREDENTIALS_PATH = './credentials.json';
 const TOKEN_PATH = './token.json';
 
