848a8236c4d4a568e205ef7814af7eb060770f09
Siem Lasseel <siem.linx@gmail.com>
2020-03-21 21:20:57 +0100

    who need statuses anyway

--- services/projectService.js
+++ services/projectService.js
@@ -36,69 +36,6 @@ const executors = [
     }
 ]
 
-const statuses = [
-    {
-        type: "contractSigned",
-        name: "Nog niet actief",
-        filter: true
-    },
-    {
-        type: "toContact",
-        name: "Te contacteren",
-        filter: true
-    },
-    {
-        type: "toPlan",
-        name: "Te plannen",
-        filter: true
-    },
-    {
-        type: "proposalSent",
-        name: "Voorstel doorgegeven",
-        filter: true
-    },
-    {
-        type: "planned",
-        name: "Ingepland",
-        filter: true
-    },
-    {
-        type: "onHold",
-        name: "On - Hold",
-        filter: true
-    },
-    {
-        type: "onHoldByClient",
-        name: "On - Hold door klant",
-        filter: true
-    },
-    {
-        type: "executed",
-        name: "Uitgevoerd",
-        filter: true
-    },
-    {
-        type: "reportAvailable",
-        name: "Rapport beschikbaar",
-        filter: true
-    },
-    {
-        type: "conformityAvailable",
-        name: "Conformiteit beschikbaar",
-        filter: true
-    },
-    {
-        type: "completed",
-        name: "Afgerond",
-        filter: true
-    },
-    {
-        type: "deleted",
-        name: "Verwijderd",
-        filter: false
-    }
-]
-
 exports.executorName = (type) => {
     let name;
     executors.forEach(executor => {
