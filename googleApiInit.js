959fd52f198cfdd7afce8b2800fa4a7e0a0fcf9c
Siem Lasseel <slasseel@selligent.com>
2020-07-04 11:41:08 +0200

    Update api init to show calendars

--- googleApiInit.js
+++ googleApiInit.js
@@ -13,7 +13,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), listCalendars);
 });
 
 /**
@@ -70,25 +70,14 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
+function listCalendars(auth) {
   const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
+  calendar.calendarList.list({
+
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
+      for (const {summary, summaryOverride, id} of res.data.items) {
+        console.log(summary, summaryOverride || '', id);
+      }
   });
 }
