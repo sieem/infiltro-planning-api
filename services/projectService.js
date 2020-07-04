7e3d09c2925d67f7560b752e158856efe09b6b13
Siem Lasseel <slasseel@selligent.com>
2020-07-04 11:40:59 +0200

    Add saving of hours in db for correct sorting

--- services/projectService.js
+++ services/projectService.js
@@ -125,7 +125,7 @@ exports.saveCalendarItem = async (project, foundProject) => {
 
             try {
                 const { eventId, calendarId, calendarLink } = await calendar.updateEvent(foundProject.calendarId, foundProject.eventId, project.executor, event);
-                project.datePlanned = moment(excistingCalendarEvent.data.start.dateTime).format("YYYY-MM-DD")
+                project.datePlanned = moment(excistingCalendarEvent.data.start.dateTime).format("YYYY-MM-DD HH:mm")
                 project.hourPlanned = moment(excistingCalendarEvent.data.start.dateTime).format("HH:mm")
                 project.eventId = eventId
                 project.calendarId = calendarId
